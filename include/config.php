8a673181e400437e7e063a42ef57c2a0c1e96711
Guillaume Baudry <baudry.guill@gmail.com>
2017-07-06 16:43:32 +0200

    add the addlab page + functions with SQL request around the lab

--- include/config.php
+++ include/config.php
@@ -2,8 +2,8 @@
 	$privateKey = "thisisaprivatekey";
 	$DB_Host = "localhost" ;
 	$DB_User = "root" ;
-	$DB_Password = "mame";
-	$DB_Database = "FunTool" ;
+	$DB_Password = "";
+	$DB_Database = "funtool" ;
 	$DB_Port = 3306 ;
 
 ?>
---
+++ view/lab/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+// function addLab($labName, $labDescription)
+//     {
+//         global $DB_DB;
+//         $sql = "INSERT INTO Lab (labName, labDescription) VALUES ('$labName', '$labDescription')";
+//         $result = $DB_DB->query($sql);
+//         return $result;
+//     }
+        
+function addLab ($labName, $labDescription)
+    {
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("INSERT INTO Lab (labName, labDescription) VALUES (?, ?)");
+        $stmt->bindParam(1, $labName);
+        $stmt->bindParam(2, $labDescription);
+        $stmt->execute();
+    }
+
+
+function deleteLab($idLab)
+    {
+        global $DB_DB;
+        $sql = "DELETE FROM Lab WHERE idLab=$idLab";
+        $result = $DB_DB->query($sql);
+    }
+
+function updateLab($idLab, $labName, $labDescription)
+    {
+        global $DB_DB;
+        $sql = "UPDATE Lab SET labName = '$labName', labDescription = '$labDescription' WHERE idLab=$idLab";
+        $result = $DB_DB->query($sql);
+    }
+
+function listAllLab()
+    {
+        global $DB_DB;
+        $sql = "SELECT * FROM Lab ORDER BY labName";
+        $result = $DB_DB->query($sql);
+        return $result;
+    }
+?>
