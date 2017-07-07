b433650e8843f5f80ae1b2915bd8a7fb2f3a059d
SeimuPVE <paul.vanelsue@gmail.com>
2017-07-07 11:22:12 +0200

    Adding corrections by Emmanuel ;)

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+	ini_set('display_errors', 1);
+
 	include("include/config.php");
+	include("include/module.php");
 	include("include/db.php");
 	include("module/pages.php");
 
--- view/home/index.php
+++ view/home/index.php
@@ -1,1 +1,5 @@
-hello
+<?php
+    loadModules("lab");
+    echo listAllLab();
+
+?>
