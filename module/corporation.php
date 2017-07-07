dbe273a80bc5718f079bd50cf8728ec28b2a468a
Guillaume Baudry <baudry.guill@gmail.com>
2017-07-07 16:56:06 +0200

    Ajout module Corporation + nouveau MCD + modif module Lab

---
+++ module/corporation.php
@@ -0,0 +1,90 @@
+<?php
+
+function addCorporation($corporateName, $logo, $telephone, $adressL1, $adressL2,$adressL3,$zipCode, $town, $country, $email, $nbFunnies)
+    {
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("INSERT INTO Lab (corporateName, logo, telephone, adressL1, adressL2, adressL3, zipCode, town, country, email, nbFunnies) VALUES (:corporateName, :logo, :telephone, :adressL1, :adressL2, :adressL3, :zipCode, :town, :country, :email, :nbFunnies)");
+
+
+    try {
+        $stmt->execute(array(
+            'corporateName' => $_POST['corporateName'],
+            'logo' => $_POST['logo'],
+            'telephone' => $_POST['telephone'],
+            'adressL1' => $_POST['adressL1'],
+            'adressL2' => $_POST['adressL2'],
+            'adressL3' => $_POST['adressL3'],
+            'zipCode' => $_POST['zipCode'],
+            'town' => $_POST['town'],
+            'country' => $_POST['country'],
+            'email' => $_POST['email'],
+            'nbFunnies' => $_POST['nbFunnies'],            
+        ));
+
+        echo "Corporation Added !";
+    }
+
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
+
+
+function deleteCorporation($idCorporation)
+    {
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("DELETE FROM corporation WHERE idCorporation=:idCorporation");
+    try {
+        $request->execute(array(
+            'idCorporation' => $_POST['idCorporation']
+        ));
+
+        echo "Corporation deleted !";
+    }
+        
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
+
+function updateCorporation($idCorporation, $corporateName, $logo, $telephone, $adressL1, $adressL2,$adressL3,$zipCode, $town, $country, $email, $nbFunnies)
+{    
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("UPDATE Lab SET corporateName = :corporateName, logo = :logo, telephone = :telephone, adressL1 = :adressL1, adressL2 = :adressL2, adressL3 = :adressL3, zipCode = :zipCode , town = :town, country = :country, email = :email, nbFunnies = :nbFunnies WHERE idCorporation = :idCorporation");
+            try
+                {
+        $stmt->execute(array(
+            'corporateName' => $_POST['corporateName'],
+            'logo' => $_POST['logo'],
+            'telephone' => $_POST['telephone'],
+            'adressL1' => $_POST['adressL1'],
+            'adressL2' => $_POST['adressL2'],
+            'adressL3' => $_POST['adressL3'],
+            'zipCode' => $_POST['zipCode'],
+            'town' => $_POST['town'],
+            'country' => $_POST['country'],
+            'email' => $_POST['email'],
+            'nbFunnies' => $_POST['nbFunnies'],            
+        ));
+            echo "Corporation updated !";
+                }
+        
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
+
+function listAllCorporation()
+    {
+        global $DB_DB;
+        $sql = "SELECT * FROM corporation";
+        $result = $DB_DB->query($sql);
+        return $result;
+    }
+?>
--- module/lab.php
+++ module/lab.php
@@ -1,36 +1,68 @@
 <?php
 
 function addLab ($labName, $labDescription)
-{
-    global $DB_DB;
-    $stmt = $DB_DB->prepare("INSERT INTO Lab (labName, labDescription) VALUES (?, ?)");
-    $stmt->bindParam(1, $labName);
-    $stmt->bindParam(2, $labDescription);
-    $stmt->execute();
-}
+    {
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("INSERT INTO Lab (labName, labDescription) VALUES (?, ?)");
+        $stmt->bindParam(1, $labName);
+        $stmt->bindParam(2, $labDescription);
+            try
+                {
+                    $stmt->execute();
+                    echo "Lab added";
+                }
+
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
+
 
 function deleteLab($idLab)
-{
-    global $DB_DB;
-    $sql = "DELETE FROM Lab WHERE idLab=$idLab";
-    $result = $DB_DB->query($sql);
-    return $result;
-}
+    {
+        global $DB_DB;
+        $stmt = $DB_DB->prepare("DELETE FROM Lab WHERE idLab=?");
+        $stmt->bindParam(1, $idLab);
+            try
+                {
+                    $stmt->execute();
+                    echo "Lab deleted";
+                }
+        
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
 
 function updateLab($idLab, $labName, $labDescription)
-{
-    global $DB_DB;
-    $sql = "UPDATE Lab SET labName = '$labName', labDescription = '$labDescription' WHERE idLab=$idLab";
-    $result = $DB_DB->query($sql);
-    return $result;
-}
+    {
+        global $DB_DB;
+        $sql = "UPDATE Lab SET labName =?, labDescription =? WHERE idLab=?";
+        $stmt->bindParam(1, $labName);
+        $stmt->bindParam(2, $labDescription);
+        $stmt->bindParam(3, $idLab);
+            try
+                {
+                    $stmt->execute();
+                    echo "Lab Updated";
+                }
+        
+            catch(Exception $e)
+                {
+                    echo $e;
+                    exit;
+                }
+    }
 
 function listAllLab()
-{
-    global $DB_DB;
-    $sql = "SELECT * FROM Lab ORDER BY labName";
-    $result = $DB_DB->query($sql);
-    return $result ;
-}
-
+    {
+        global $DB_DB;
+        $sql = "SELECT * FROM Lab";
+        $result = $DB_DB->query($sql);
+        return $result;
+    }
 ?>
