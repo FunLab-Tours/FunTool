9a8d0bab2605e4997e4c2ec4729bb6b7ae07db69
SeimuPVE <paul.vanelsue@gmail.com>
2018-04-12 22:25:35 +0200

    Adding ignored configuration files to use it on a new setup.

---
+++ ignored_config_files/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$DB_Host = "localhost";
+$DB_User = "root";
+$DB_Password = ""; // TODO : be careful to change the password when you setup the website on a server.
+$DB_Database = "FunTool";
+$DB_Port = 3306;
+
+$privateKey = "thisisaprivatekey"; // TODO : be careful to change the default private key when you setup the website on a server.
+
+$DEFAULT_FUNNIES = 5;
+$DEBUG_MODE = true; // TODO : be careful to change that to false when you setup the website on a server.
---
+++ ignored_config_files/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Titre de la page</title> <!-- TODO : change page title. -->
+	<base href="http://localhost/FunTool/"> <!-- TODO : change base -->
+</head>
+<body>
